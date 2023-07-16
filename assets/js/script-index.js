8b1a09c004eb7670ade0ff15c01c20049c2cba91
RanggaCasper <hyfanutama@gmail.com>
2023-07-16 22:27:23 +0800

    kayaknya sih udah

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -15,3 +15,25 @@ closeButton.addEventListener('click', () => {
 const popup = document.querySelector('.popup-album');
 popup.style.display = 'none';
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const navbarLinks = document.querySelectorAll('.link-item');
+  
+  function setActiveLink() {
+    const currentHash = window.location.hash;
+    
+    navbarLinks.forEach(link => {
+      link.classList.remove('active');
+      
+      if (currentHash === '' && link.getAttribute('href') === '#') {
+        link.classList.add('active');
+      } else if (link.getAttribute('href') === currentHash) {
+        link.classList.add('active');
+      }
+    });
+  }
+  
+  setActiveLink();
+  
+  window.addEventListener('hashchange', setActiveLink);
+});
